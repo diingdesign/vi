05d699223b0dda3b8d56c1dfa3d5c8c31958471c
Paper Lee <paper@diing.com>
2020-03-30 15:38:47 -0700

    Updates

---
+++ precache-manifest.769e8d8583286eb965c9c3816571b394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159d71b127dafa740b5125674f3e4048",
+    "url": "/vi/index.html"
+  },
+  {
+    "revision": "99bbb4dad6e12a9ef6d4",
+    "url": "/vi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0aa653ddd150d51d1350",
+    "url": "/vi/static/js/2.da913cb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vi/static/js/2.da913cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bbb4dad6e12a9ef6d4",
+    "url": "/vi/static/js/main.68ad80c1.chunk.js"
+  },
+  {
+    "revision": "51a2ef12de0d19819a9a",
+    "url": "/vi/static/js/runtime-main.316eca87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vi/static/media/logo.5d5d9eef.svg"
+  }
+]);
