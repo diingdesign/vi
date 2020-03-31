f6c67aed71e128c68d12aa7c0bc887e62d9ddf9d
Paper Lee <paper@diing.com>
2020-03-30 23:13:56 -0700

    Add chapter 2 models. Move data to separate js file

---
+++ src/Data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        title: "As the Old Sing, So Pipe the Young",
+        artist: "Jan Steen, 1668-1670",
+        model: "01"
+    },
+    {
+        title: "The Goldfinch",
+        artist: "Carel Fabritius, 1654",
+        model: "02"
+    },
+    {
+        title: "Smiling Girl, a Courtesan, Holding an Obscene Image",
+        artist: "Gerrit van Honthorst, 1625",
+        model: "03"
+    },
+    {
+        title: "Mistress and Maid",
+        artist: "Johannes Vermeer, 1667",
+        model: "04"
+    },
+    {
+        title: "The Portrait",
+        artist: "René Magritte, 1935",
+        model: "05"
+    },
+    {
+        title: "Building in New York",
+        artist: "",
+        model: "06"
+    }
+];
+
+export default data;
--- src/Page.js
+++ src/Page.js
@@ -6,29 +6,7 @@ import {
   Link,
   useParams
 } from "react-router-dom";
-
-export const data = [
-  {
-    title: "As the Old Sing, So Pipe the Young",
-    artist: "Jan Steen, 1668-1670",
-    model: "01"
-  },
-  {
-    title: "The Goldfinch",
-    artist: "Carel Fabritius, 1654",
-    model: "02"
-  },
-  {
-    title: "Smiling Girl, a Courtesan, Holding an Obscene Image",
-    artist: "Gerrit van Honthorst, 1625",
-    model: "03"
-  },
-  {
-    title: "Mistress and Maid",
-    artist: "Johannes Vermeer, 1667",
-    model: "04"
-  }
-];
+import data from './Data';
 
 function Toolbar() {
   var { pageId } = useParams();
